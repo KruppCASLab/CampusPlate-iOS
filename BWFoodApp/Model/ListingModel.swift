3df5dc8693a552aaf03121798d889bc298df020f
Brian Krupp <bkrupp@bw.edu>
2018-08-20 10:09:21 -0400

    Rearranged items and added stub for model

---
+++ BWFoodApp/Model/ListingModel.swift
@@ -0,0 +1,13 @@
+//
+//  ListingModel.swift
+//  BWFoodApp
+//
+//  Created by Brian Krupp on 8/20/18.
+//  Copyright © 2018 Dan Fitzgerald. All rights reserved.
+//
+
+import Foundation
+
+class ListingModel {
+    
+}
--- BWFoodApp/Types/Listing.swift
+++ BWFoodApp/Types/Listing.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Foundation
 
-class CreateListing {
+class Listing {
     var location = ""
     var food = ""
     var time = ""
