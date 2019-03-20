2e1e9d8f54b1e3077781be5c747d61b42f31d48c
Dan <thetribe41@gmail.com>
2019-03-19 21:32:06 -0400

    Dan

---
+++ BWFoodApp/Controller/StepperTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StepperTableViewCell.swift
+//  BWFoodApp
+//
+//  Created by Dan Fitzgerald on 3/19/19.
+//  Copyright © 2019 Dan Fitzgerald. All rights reserved.
+//
+
+import UIKit
+
+class StepperTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
