c4b205070d2b7a6adaaf258e9eaf154f72acbb9e
Dan <thetribe41@gmail.com>
2021-02-18 15:48:31 -0500

    Created basic confirmation screen with mock data

---
+++ BWFoodApp/Controller/PickUpConfirmationViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PickUpConfirmationViewController.swift
+//  BWFoodApp
+//
+//  Created by Dan Fitzgerald on 2/18/21.
+//  Copyright © 2021 Dan Fitzgerald. All rights reserved.
+//
+
+import UIKit
+
+class PickUpConfirmationViewController: UIViewController {
+    
+    @IBOutlet weak var CircleView: UIView!
+    @IBOutlet weak var FoodStopLocation: UITextView!
+    @IBOutlet weak var FoodStopAddress: UILabel!
+    @IBOutlet weak var FoodStopIdNumber: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        CircleView.layer.cornerRadius = CircleView.frame.size.width/2
+    }
+    
+ 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
