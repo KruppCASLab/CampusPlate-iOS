4fcaaf81371cf0e003b802674fa5cbef26231f6d
Brian Krupp <bkrupp@bw.edu>
2021-04-07 10:39:41 -0400

    Cleaned up code on segue checker

--- BWFoodApp/Controller/SegueCheckerViewController.swift
+++ BWFoodApp/Controller/SegueCheckerViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SegueCheckerViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,29 +18,10 @@ class SegueCheckerViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-             //        if let cred = CredentialManager.getCredential(){
-        //        //            performSegue(withIdentifier: "toSignUpScreen", sender: nil)
-        //        //        }else{
-        //        //            performSegue(withIdentifier: "toApplication", sender: nil)
-        //        //
-        //        //        }
-        //
-                if CredentialManager.getCredential() != nil {
-                    performSegue(withIdentifier: "toApplication", sender: nil)
-                }else{
-                    performSegue(withIdentifier: "toSignUpScreen", sender: nil)
-                }
+        if CredentialManager.getCredential() != nil {
+            performSegue(withIdentifier: "toApplication", sender: nil)
+        }else{
+            performSegue(withIdentifier: "toSignUpScreen", sender: nil)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
