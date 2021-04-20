da35585c29d357f3ebdc88956afc337f943ab5c6
Brian Krupp <bkrupp@bw.edu>
2021-04-20 14:21:19 -0400

    Added ability to refresh food listings periodically so that user does not have to force close app and restart it. Used a timer with observer pattern. Did not use notification approach as it uses a selector and there is no check if the method actually exists.

--- BWFoodApp/AppDelegate.swift
+++ BWFoodApp/AppDelegate.swift
@@ -12,11 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let appState = AppState.shared
     
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-    }
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
@@ -27,7 +24,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        appState.notifyApplicationDidBecomeInactive()
     }
+    
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
@@ -40,6 +40,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        appState.notifyApplicationDidBecomeActive()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
