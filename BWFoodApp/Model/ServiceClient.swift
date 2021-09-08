db6d3a3040b8de63c78a57063e4bcbefaf2dc333
Julia Gersey <jgersey20@bw.edu>
2021-09-08 11:35:27 -0400

    URL centralized by ServiceClient class

---
+++ BWFoodApp/Model/ServiceClient.swift
@@ -0,0 +1,20 @@
+//
+//  ServiceClient.swift
+//  BWFoodApp
+//
+//  Created by Julia  Gersey on 9/5/21.
+//  Copyright © 2021 Campus Plate - BW. All rights reserved.
+//
+
+import Foundation
+
+
+class ServiceClient {
+    
+    static func serviceClientUrl() -> String {
+        let url = "https://mopsdev.bw.edu/cp/rest.php/"
+        
+        return String(url)
+        
+    }
+}
