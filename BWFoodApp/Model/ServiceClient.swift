3c3a99fea6a39b992bcc3f40c822e9ae37b0bab5
Julia Gersey <jgersey20@bw.edu>
2021-09-08 12:21:38 -0400

    centralizing service point URL

--- BWFoodApp/Model/ServiceClient.swift
+++ BWFoodApp/Model/ServiceClient.swift
@@ -12,7 +12,7 @@ import Foundation
 class ServiceClient {
     
     static func serviceClientUrl() -> String {
-        let url = "https://mopsdev.bw.edu/cp/rest.php/"
+        let url = "https://mopsdev.bw.edu/cp/rest.php"
         
         return String(url)
         
